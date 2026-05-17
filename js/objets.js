6deff1cc034086f89a53401fe745cceda69518ff
Luis Reyes <luisrafaelreyescaro1@gmail.com>
2026-05-17 00:43:12 -0500

    first commit, but this code is going to be just for me

---
+++ js/objets.js
@@ -0,0 +1,35 @@
+class Messages {
+    constructor(){
+        this.container = document.querySelector(".container");
+        this.message = document.querySelector(".message");
+        this.yesBtn = document.querySelector(".yes");
+        this.notBtn = document.querySelector(".not");
+        this.loadingArrow = document.querySelector(".deleting");
+    }
+
+    confirmPopUp(callback){
+        this.container.classList.toggle("show");
+
+        return new Promise((resolve) => {
+
+            this.notBtn.onclick = () => {
+                this.container.classList.remove("show");
+                resolve(false);
+            };
+
+            this.yesBtn.onclick = () => {
+                this.message.style.display = "none";
+                this.loadingArrow.style.display = "flex";
+                
+                setTimeout(() => {
+                    this.container.classList.remove("show");
+                    this.message.style.display = "grid"; 
+                    this.loadingArrow.style.display = "none";
+                    resolve(true);
+                },800);
+            }
+        });
+    }
+}
+
+export default Messages;
---
+++ js/script.js
@@ -0,0 +1,48 @@
+import Messages from "./objets.js";
+
+const mes = new Messages();
+
+const formNote = document.querySelector("form");
+const inputNote = formNote.querySelector(".input");
+const notes = document.querySelector(".listNotes");
+
+document.addEventListener("DOMContentLoaded", () => {
+    notes.innerHTML = localStorage.getItem("data") || "";
+});
+
+formNote.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let data = inputNote.value.trim();
+    inputNote.value = "";
+    if(data != "") {
+        createNote(notes, data);
+        console.log("New note added");
+    } else { 
+        console.error("Cannot create an Empty note");
+        return;
+    }
+});
+
+function createNote(notes, data) {
+    notes.innerHTML += `
+    <li class="liNote">
+        <button class="deleteNote">x</button>
+        <p>${data}</p>
+    </li>
+    `
+    localStorage.setItem("data", notes.innerHTML);
+};
+
+notes.addEventListener("click", async (e) => {
+    if (e.target.classList.contains("deleteNote")) {
+        const liNote = e.target.parentElement;
+        const confirmed = await mes.confirmPopUp();
+        if(confirmed){
+            notes.removeChild(liNote);
+            localStorage.setItem("data", notes.innerHTML);
+            console.log("Note deleted from DOM and LocalStorage");
+        }
+    }
+});
+
+
